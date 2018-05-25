65bc5e8d770ee6f7788c3956033faad1fbe47231
Maanush2004 <p.maanush@gmail.com>
2018-05-25 22:01:24 +0530

    Update framework and libs

--- manning-vendor.mk
+++ manning-vendor.mk
@@ -17,6 +17,8 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/manning/bin,
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/manning/etc,system/etc)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/manning/framework,system/framework)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/manning/lib,system/lib)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/manning/lib64,system/lib64)
